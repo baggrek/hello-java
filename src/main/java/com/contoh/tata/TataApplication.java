4d3211a96ff0d2852e6fc48d51327902d88c40c5
randy pradipta <baggrek@gmail.com>
2018-10-14 08:27:32 +0700

    first commit

---
+++ src/main/java/com/contoh/tata/TataApplication.java
@@ -0,0 +1,12 @@
+package com.contoh.tata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TataApplication.class, args);
+	}
+}
